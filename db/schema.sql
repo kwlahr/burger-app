806c755dc0f72c742c2e5e5e92df31fc8d9cc42d
kwlahr <kwlahr@gmail.com>
2019-12-22 14:43:34 -0700

    fixed filepaths to target app.js and cleaned up routes code

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,2 @@
-CREATE DATABASE burgers_db;
-USE burgers_db;
-
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name VARCHAR(255) NOT NULL,
-	devoured BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
-);
+INSERT INTO burgers(burger_name, devoured)
+VALUES('Bacon Burger', false),('Cheeseburger', false), ('Royale with Cheese', true);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (name) VALUES ('bacon double cheeseburger');
 INSERT INTO burgers (name) VALUES ('whiskey bacon burger');
-INSERT INTO burgers (name, devoured) VALUES ('smoked brie burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Royale with Cheese', true);
