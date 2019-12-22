269f55cf58fd97872d3320cf67bf177d15951144
kwlahr <kwlahr@gmail.com>
2019-12-22 14:05:13 -0700

    cleaned up boilerplate code and added styling

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,14 +1,12 @@
 var express = require("express");
-
 var router = express.Router();
-
 // Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.all(function(data) {
-    var hbsObject = {
+    const hbsObject = {
       burgers: data
     };
     console.log(hbsObject);
@@ -17,32 +15,39 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.create(["name", "devoured"], [req.body.name, req.body.devoured], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  });
+  burger.create(req.body.burger, function (result) {
+        res.redirect('/');
+    })
 });
 
-router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+router.put('/api/burgers/:id', function (req, res) {
+    let condition = req.params.id;
 
-  console.log("condition", condition);
-
-  burger.update(
-    {
-      devoured: req.body.devoured
-    },
-    condition,
-    function(result) {
-      if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      }
-      res.status(200).end();
-
-    }
-  );
+    burger.update(condition, function (result) {
+        res.status(200).end();
+    })
 });
+
+// router.put("/api/burgers/:id", function(req, res) {
+//   var condition = "id = " + req.params.id;
+
+//   console.log("condition", condition);
+
+//   burger.update(
+//     {
+//       devoured: req.body.devoured
+//     },
+//     condition,
+//     function(result) {
+//       if (result.changedRows === 0) {
+//         // If no rows were changed, then the ID must not exist, so 404
+//         return res.status(404).end();
+//       }
+//       res.status(200).end();
+
+//     }
+//   );
+// });
 
 // Export routes for server.js to use.
 module.exports = router;
--- server.js
+++ server.js
@@ -1,27 +1,22 @@
-const express = require("express");
-const exphbs = require("express-handlebars");
-const mysql = require("mysql");
-const mysql2 = require("mysql2");
+const express = require('express');
+const PORT = process.env.PORT || 8080;
 
-var PORT = process.env.PORT || 3000;
+const app = express();
 
-var app = express();
+app.use(express.static(__dirname + '/public'));
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
-// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+const exphbs = require('express-handlebars');
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+const routes = require('./controllers/burgers_controller');
 
 app.use(routes);
 
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+app.listen(PORT, function () {
+    console.log(`express server listening on: http://localhost:${PORT}`);
 });
