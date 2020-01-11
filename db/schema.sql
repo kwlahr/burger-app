3f172cdbb1213db197fec9335e5f01510b2e8025
kwlahr <kwlahr@gmail.com>
2020-01-10 21:36:14 -0700

    added jawsdb table schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,11 @@
+USE `fx5nmcsw011hawze`;
+CREATE TABLE burgers (
+    id INT AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(100),
+    devoured BOOLEAN,
+    createdAt TIMESTAMP NOT NULL,
+    PRIMARY KEY(id)
+);
+
 INSERT INTO burgers(burger_name, devoured)
 VALUES('Bacon Burger', false),('Cheeseburger', false), ('Royale with Cheese', true);
